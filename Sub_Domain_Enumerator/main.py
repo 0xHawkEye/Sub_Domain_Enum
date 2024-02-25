1904c4773c7ffe6076cf7cc9758f6108aa76c963
AyushKr41 <rajayush4422+git@gmail.com>
2024-02-26 04:44:09 +0530

    files

---
+++ Sub_Domain_Enumerator/main.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def request(url):
+    try:
+        result = requests.get("https://" + url)
+        if result:
+            print("[+] Subdomain discovered ------->  " + url)
+
+    except:
+        pass
+
+
+def main():
+    target_url = input("Enter the target url : ")
+    with open("subdmainwordlist.txt", "r") as wordlist:
+        for line in wordlist:
+            word = line.strip()
+            test_url = word + "." + target_url
+            request(test_url)
+
+
+main()
